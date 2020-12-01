3aeff46e4c13e1ec86f351e8392fc73707d93e9f
CorinaZanfir <zanfircorina@gmail.com>
2020-12-01 20:16:09 +0200

    delete employee and manager classes

--- src/com/team4/employeemood/Employee.java
+++
@@ -1,8 +0,0 @@
-package com.team4.employeemood;
-
-public class Employee extends User {
-    public Employee(String firstName, String lastName, String birthDate, String employmentDate, String projectName) {
-        super(firstName, lastName, birthDate, employmentDate, projectName);
-    }
-
-}
--- src/com/team4/employeemood/Manager.java
+++
@@ -1,9 +0,0 @@
-package com.team4.employeemood;
-
-public class Manager extends User {
-
-    public Manager(String firstName, String lastName, String birthDate, String employmentDate, String projectName) {
-        super(firstName, lastName, birthDate, employmentDate, projectName);
-    }
-
-}
