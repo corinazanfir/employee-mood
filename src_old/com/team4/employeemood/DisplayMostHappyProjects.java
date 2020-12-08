6d321b0250485c0a9945b2d55dfcac15a117e29e
Catalin Gh <catalingheorghe111@gmail.com>
2020-12-08 16:56:56 +0200

    old src cleanup

--- src_old/com/team4/employeemood/DisplayMostHappyProjects.java
+++
@@ -1,5 +0,0 @@
-package com.team4.employeemood;
-
-public class DisplayMostHappyProjects {
-
-}
--- src_old/com/team4/employeemood/Exceptions/PatternNotMatchingException.java
+++
@@ -1,5 +0,0 @@
-package com.team4.employeemood.Exceptions;
-
-public class PatternNotMatchingException extends Throwable {
-    public PatternNotMatchingException(String message) { super (message);}
-}
