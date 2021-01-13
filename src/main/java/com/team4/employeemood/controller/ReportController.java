bfb03f2ff0f81469a912f8a1de8da61794ba92b2
Catalin Gh <catalingheorghe111@gmail.com>
2021-01-13 14:14:35 +0200

    ReportController update & cleanup
    TeamAverageReportRepresentation completed 3 TODOs - final

--- src/main/java/com/team4/employeemood/controller/ReportController.java
+++ src/main/java/com/team4/employeemood/controller/ReportController.java
@@ -1,6 +1,5 @@
 package com.team4.employeemood.controller;
 
-import com.team4.employeemood.Mood;
 import com.team4.employeemood.controller.representation.TeamAverageReportRepresentation;
 import com.team4.employeemood.repository.MoodRepository;
 import com.team4.employeemood.service.ReportService;
@@ -12,36 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.List;
 
 @RestController
 public class ReportController {
-    @Autowired
-    private UserService userService;
 
     @Autowired
     private ReportService reportService;
 
-    @Autowired
-    private MoodRepository moodRepository;
-
-    @GetMapping("/api/v1/reports/users")
-    public Integer getNumberOfEmployeesAssignedToProjectBetweenDates(@RequestParam Long projectId, @RequestParam String startDate, @RequestParam String endDate) throws ParseException {
-
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-
-        return userService.getListOfUsersAssignedToProjectBetweenDates(projectId, sdf.parse(startDate), sdf.parse(endDate)).size();
-    }
-
-    @GetMapping("/api/v1/reports/moodsForUsers")
-    public List<Mood> getMoodsForUsers(@RequestParam Long projectId, @RequestParam String startDate, @RequestParam String endDate) throws ParseException {
-
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-
-        List<Long> totalNumberOfTeamMembers = userService.getListOfUsersAssignedToProjectBetweenDates(projectId, sdf.parse(startDate), sdf.parse(endDate));
-        return moodRepository.findByUserIdIn(totalNumberOfTeamMembers);
-
-    }
+    // Sample to test - localhost:8080/api/v1/reports/teamAverage?projectId=1&startDate=2018-01-01&endDate=2021-01-15
 
     @GetMapping("/api/v1/reports/teamAverage")
     public TeamAverageReportRepresentation generateTeamAverage(@RequestParam Long projectId, @RequestParam String startDate, @RequestParam String endDate) throws ParseException {
